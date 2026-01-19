6c77b620e8e64980538f7c9ba14b700494bb6893
SOIL MERCHANTS PROPERTY DEVELOPERS <soilmerchantskenya@gmail.com>
2026-01-19 13:22:29 +0000

    ## Error Type Console GenkitError
    ## Error Message
    NOT_FOUND: Model 'ge

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import {vertexAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [vertexAI({location: "us-central1"})],
-  model: 'gemini-1.5-flash-latest',
+  model: 'gemini-1.5-flash',
 });
