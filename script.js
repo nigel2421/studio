d9f000df340cc6ddc4f3ec60c5dfdf2bcaf09c2f
SOIL MERCHANTS PROPERTY DEVELOPERS <soilmerchantskenya@gmail.com>
2026-02-01 06:49:48 +0000

    VU

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 500,
-  duration: '5m',
+  vus: 200,
+  duration: '2m',
 };
 
 export default function () {
