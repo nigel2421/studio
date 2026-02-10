c305439dafe80ed08ab3807843bdc6d1b11f3522
Henry Wambua <wambuahenry15@gmail.com>
2026-02-10 12:22:43 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  },
   /* config options here */
   images: {
     remotePatterns: [
